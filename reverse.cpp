8f04ef2264037da5bb6c1cf868d7190512a2394a
Tapan Tyagi <tapantyagi07@gmail.com>
2026-07-06 23:59:36 +0530

    Implement reverse integer function in Solution class

---
+++ reverse.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long revnum = 0;
+        while (x) {
+            int ld = x % 10;
+            revnum = (revnum * 10) + ld;
+            x = x / 10;
+        }
+        
+        if(revnum>INT_MAX || revnum<INT_MIN){
+            return 0;
+        }
+        
+        
+        return revnum;
+    }
+};
